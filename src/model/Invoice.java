96e80a46a1a46a0046903cff3df507d7f293ac38
Ted2121 <89663563+Ted2121@users.noreply.github.com>
2022-03-21 19:24:27 +0100

    Created fields, constructor, accessors for Invoice class

--- src/model/Invoice.java
+++ src/model/Invoice.java
@@ -5,5 +5,54 @@ public class Invoice {
 	private String invoiceNo;
 	private String paymentDate;
 	private double amount;
+	
+	public Invoice(int id, String invoiceNo, String paymentDate, double amount) {
+		
+		this.id = id;
+		this.invoiceNo = invoiceNo;
+		this.paymentDate = paymentDate;
+		this.amount = amount;
+	}
 
+	public Invoice(String invoiceNo, String paymentDate, double amount) {
+		
+		this.invoiceNo = invoiceNo;
+		this.paymentDate = paymentDate;
+		this.amount = amount;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getInvoiceNo() {
+		return invoiceNo;
+	}
+
+	public void setInvoiceNo(String invoiceNo) {
+		this.invoiceNo = invoiceNo;
+	}
+
+	public String getPaymentDate() {
+		return paymentDate;
+	}
+
+	public void setPaymentDate(String paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	
+	
 }
