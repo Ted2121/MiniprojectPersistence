7b6aeece58b4b26f181ad6df14265c0518492afe
Ted2121 <89663563+Ted2121@users.noreply.github.com>
2022-03-21 18:54:08 +0100

    Created field, constructors and accessors for Clothing class

--- src/model/Clothing.java
+++ src/model/Clothing.java
@@ -1,5 +1,40 @@
 package model;
 
-public class Clothing {
+public class Clothing extends Product{
+	
+	private String size;
+	private String color;
+	
+	public Clothing(String name, double purchasePrice, double salePrice, String countryOfOrigin, int minStock,
+			String size, String color) {
+		super(name, purchasePrice, salePrice, countryOfOrigin, minStock);
+		this.size = size;
+		this.color = color;
+	}
+	
+	public Clothing(int id, String name, double purchasePrice, double salePrice, String countryOfOrigin, int minStock,
+			String size, String color) {
+		super(id, name, purchasePrice, salePrice, countryOfOrigin, minStock);
+		this.size = size;
+		this.color = color;
+	}
 
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+	
 }
