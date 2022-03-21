ef2a90daece2994158920fce143f4191a31d970d
Ted2121 <89663563+Ted2121@users.noreply.github.com>
2022-03-21 17:11:55 +0100

    Created fields in the SaleOrder class

--- src/model/SaleOrder.java
+++ src/model/SaleOrder.java
@@ -1,5 +1,84 @@
 package model;
 
-public class SaleOrder {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+public class SaleOrder {
+	private static int id;
+	private String orderDate;
+	private String deliveryDate;
+	private boolean deliveryStatus;
+	private Customer customer;
+	private Invoice invoice;
+	
+	private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+	
+	
+	
+	public SaleOrder(String orderDate, String deliveryDate, boolean deliveryStatus, Customer customer,
+			Invoice invoice) {
+	
+		this.orderDate = LocalDateTime.now().format(getFormat());
+		this.deliveryDate = LocalDateTime.now().plusDays(14).format(getFormat());
+		this.deliveryStatus = deliveryStatus;
+		this.customer = customer;
+		this.invoice = invoice;
+	}
+
+
+    public String getOrderDate() {
+		return orderDate;
+	}
+
+
+	public void setOrderDate(String orderDate) {
+		this.orderDate = orderDate;
+	}
+
+
+	public String getDeliveryDate() {
+		return deliveryDate;
+	}
+
+
+	public void setDeliveryDate(String deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
+
+
+	public boolean isDeliveryStatus() {
+		return deliveryStatus;
+	}
+
+
+	public void setDeliveryStatus(boolean deliveryStatus) {
+		this.deliveryStatus = deliveryStatus;
+	}
+
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+
+	public Invoice getInvoice() {
+		return invoice;
+	}
+
+
+	public void setInvoice(Invoice invoice) {
+		this.invoice = invoice;
+	}
+
+
+	public static DateTimeFormatter getFormat() {
+        return format;
+    }
+    
+    
 }
