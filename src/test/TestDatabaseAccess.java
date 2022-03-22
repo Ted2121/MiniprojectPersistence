eb4a49e3d88f71c28ac77f1bced361484a2ce5dc
schesneau <10435523@ucn.dk>
2022-03-22 11:33:46 +0100

    Created database connection class and test

---
+++ src/test/TestDatabaseAccess.java
@@ -0,0 +1,56 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import data_access.DatabaseConnection;
+
+
+//import controllayer.ControlPayStation;
+//import controllayer.Currency;
+//import controllayer.IPayStation;
+//import controllayer.IReceipt;
+//import controllayer.IllegalCoinException;
+
+
+//import static org.junit.Assert.*;
+
+/**
+ * Inspired by the book: Flexible, Reliable Software Henrik B�rbak Christensen:
+ * Flexible, Reliable Software. Taylor and Francis Group, LLC 2010
+ */
+
+public class TestDatabaseAccess {
+	
+	
+	DatabaseConnection con = null;
+
+	/** Fixture for pay station testing. */
+	@Before
+	public void setUp() {
+		con = DatabaseConnection.getInstance();
+	}
+	
+	
+	@Test
+	public void wasConnected() {
+		assertNotNull("Connected - connection cannot be null", con);
+		
+		DatabaseConnection.closeConnection();
+		boolean wasNullified = DatabaseConnection.instanceIsNull();
+		assertTrue("Disconnected - instance set to null", wasNullified);
+		
+		con = DatabaseConnection.getInstance();
+		assertNotNull("Connected - connection cannot be null", con);		
+	}
+	
+	
+	/** Fixture for pay station testing. */
+	@After
+	public void cleanUp() {
+		DatabaseConnection.closeConnection();
+	}	
+	
+
+}
