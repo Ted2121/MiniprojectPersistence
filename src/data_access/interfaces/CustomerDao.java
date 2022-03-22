edb880b9968e048dee292d46a9e34a370f997139
schesneau <10435523@ucn.dk>
2022-03-22 15:34:17 +0100

    Creating all the DaoImplementation class and finishing the CustomerDao

--- src/data_access/interfaces/CustomerDao.java
+++ src/data_access/interfaces/CustomerDao.java
@@ -1,13 +1,14 @@
 package data_access.interfaces;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import model.Customer;
 
 public interface CustomerDao {
-	Customer findById(int id);
-	List<Customer> findAll();
-	int create(Customer objectToInsert);
-	void update(Customer objectToUpdate);
-	void delete(Customer objectToDelete);
+	Customer findById(int id) throws SQLException;
+	List<Customer> findAll() throws SQLException;
+	int create(Customer objectToInsert) throws SQLException;
+	boolean update(Customer objectToUpdate) throws SQLException;
+	boolean delete(Customer objectToDelete) throws SQLException;
 }
