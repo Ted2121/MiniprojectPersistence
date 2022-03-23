a367ae4ffcff2590358465df32b1588558844aba
schesneau <10435523@ucn.dk>
2022-03-23 11:46:17 +0100

    Changes in SaleOrderProduct

--- src/model/SaleOrder_Product.java
+++ src/model/SaleOrder_Product.java
@@ -3,36 +3,15 @@ package model;
 import java.util.ArrayList;
 
 public class SaleOrder_Product {
-	private ArrayList <Product> products;
-	private ArrayList <SaleOrder> saleOrders;
 	private int quantity;
 	private Product product;
 	private SaleOrder order;
 	
-	public SaleOrder_Product(ArrayList<Product> products, ArrayList<SaleOrder> saleOrders, int quantity,
-			Product product, SaleOrder order) {
+	public SaleOrder_Product(Product product, SaleOrder order, int quantity) {
 		
-		this.products = products;
-		this.saleOrders = saleOrders;
 		this.quantity = quantity;
 		this.product = product;
 		this.order = order;
-	}
-
-	public ArrayList<Product> getProducts() {
-		return products;
-	}
-
-	public void setProducts(ArrayList<Product> products) {
-		this.products = products;
-	}
-
-	public ArrayList<SaleOrder> getSaleOrders() {
-		return saleOrders;
-	}
-
-	public void setSaleOrders(ArrayList<SaleOrder> saleOrders) {
-		this.saleOrders = saleOrders;
 	}
 
 	public int getQuantity() {
